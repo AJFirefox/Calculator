f4d76d6667e510365cebdc9816dd6d9a649775ae
AJFirefox <AJFirefox08@gmail.com>
2026-09-22 10:42:52 -0500

    color

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -58,255 +58,332 @@
             // 
             // Seven
             // 
+            Seven.BackColor = Color.LightSteelBlue;
+            Seven.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Seven.Location = new Point(296, 90);
             Seven.Name = "Seven";
+            Seven.Padding = new Padding(1);
             Seven.Size = new Size(50, 30);
             Seven.TabIndex = 0;
             Seven.Text = "7";
-            Seven.UseVisualStyleBackColor = true;
+            Seven.UseVisualStyleBackColor = false;
             Seven.Click += Seven_Click;
             // 
             // Eight
             // 
+            Eight.BackColor = Color.LightSteelBlue;
+            Eight.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Eight.Location = new Point(352, 90);
             Eight.Name = "Eight";
+            Eight.Padding = new Padding(1);
             Eight.Size = new Size(50, 30);
             Eight.TabIndex = 1;
             Eight.Text = "8";
-            Eight.UseVisualStyleBackColor = true;
+            Eight.UseVisualStyleBackColor = false;
             Eight.Click += Eight_Click;
             // 
             // Nine
             // 
+            Nine.BackColor = Color.LightSteelBlue;
+            Nine.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Nine.Location = new Point(408, 90);
             Nine.Name = "Nine";
+            Nine.Padding = new Padding(1);
             Nine.Size = new Size(50, 30);
             Nine.TabIndex = 2;
             Nine.Text = "9";
-            Nine.UseVisualStyleBackColor = true;
+            Nine.UseVisualStyleBackColor = false;
             Nine.Click += Nine_Click;
             // 
             // Four
             // 
+            Four.BackColor = Color.LightSteelBlue;
+            Four.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Four.Location = new Point(296, 126);
             Four.Name = "Four";
+            Four.Padding = new Padding(1);
             Four.Size = new Size(50, 30);
             Four.TabIndex = 3;
             Four.Text = "4";
-            Four.UseVisualStyleBackColor = true;
+            Four.UseVisualStyleBackColor = false;
             Four.Click += Four_Click;
             // 
             // Five
             // 
+            Five.BackColor = Color.LightSteelBlue;
+            Five.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Five.Location = new Point(352, 126);
             Five.Name = "Five";
+            Five.Padding = new Padding(1);
             Five.Size = new Size(50, 30);
             Five.TabIndex = 4;
             Five.Text = "5";
-            Five.UseVisualStyleBackColor = true;
+            Five.UseVisualStyleBackColor = false;
             Five.Click += Five_Click;
             // 
             // Six
             // 
+            Six.BackColor = Color.LightSteelBlue;
+            Six.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Six.Location = new Point(408, 126);
             Six.Name = "Six";
+            Six.Padding = new Padding(1);
             Six.Size = new Size(50, 30);
             Six.TabIndex = 5;
             Six.Text = "6";
-            Six.UseVisualStyleBackColor = true;
+            Six.UseVisualStyleBackColor = false;
             Six.Click += Six_Click;
             // 
             // One
             // 
+            One.BackColor = Color.LightSteelBlue;
+            One.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             One.Location = new Point(296, 162);
             One.Name = "One";
+            One.Padding = new Padding(1);
             One.Size = new Size(50, 30);
             One.TabIndex = 8;
             One.Text = "1";
-            One.UseVisualStyleBackColor = true;
+            One.UseVisualStyleBackColor = false;
             One.Click += One_Click;
             // 
             // Two
             // 
+            Two.BackColor = Color.LightSteelBlue;
+            Two.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Two.Location = new Point(352, 162);
             Two.Name = "Two";
+            Two.Padding = new Padding(1);
             Two.Size = new Size(50, 30);
             Two.TabIndex = 9;
             Two.Text = "2";
-            Two.UseVisualStyleBackColor = true;
+            Two.UseVisualStyleBackColor = false;
             Two.Click += Two_Click;
             // 
             // Three
             // 
+            Three.BackColor = Color.LightSteelBlue;
+            Three.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Three.Location = new Point(408, 162);
             Three.Name = "Three";
+            Three.Padding = new Padding(1);
             Three.Size = new Size(50, 30);
             Three.TabIndex = 10;
             Three.Text = "3";
-            Three.UseVisualStyleBackColor = true;
+            Three.UseVisualStyleBackColor = false;
             Three.Click += Three_Click;
             // 
             // Zero
             // 
+            Zero.BackColor = Color.LightSteelBlue;
+            Zero.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Zero.Location = new Point(296, 198);
             Zero.Name = "Zero";
+            Zero.Padding = new Padding(1);
             Zero.Size = new Size(50, 30);
             Zero.TabIndex = 11;
             Zero.Text = "0";
-            Zero.UseVisualStyleBackColor = true;
+            Zero.UseVisualStyleBackColor = false;
             Zero.Click += Zero_Click;
             // 
             // Decimal
             // 
+            Decimal.BackColor = Color.LightSteelBlue;
+            Decimal.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Decimal.Location = new Point(352, 198);
             Decimal.Name = "Decimal";
+            Decimal.Padding = new Padding(1);
             Decimal.Size = new Size(50, 30);
             Decimal.TabIndex = 12;
             Decimal.Text = ".";
-            Decimal.UseVisualStyleBackColor = true;
+            Decimal.UseVisualStyleBackColor = false;
             // 
             // Exponent
             // 
+            Exponent.BackColor = SystemColors.GradientInactiveCaption;
+            Exponent.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Exponent.Location = new Point(408, 198);
             Exponent.Name = "Exponent";
+            Exponent.Padding = new Padding(1);
             Exponent.Size = new Size(50, 30);
             Exponent.TabIndex = 13;
             Exponent.Text = "EXP";
-            Exponent.UseVisualStyleBackColor = true;
+            Exponent.UseVisualStyleBackColor = false;
             // 
             // Divide
             // 
+            Divide.BackColor = SystemColors.GradientInactiveCaption;
+            Divide.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Divide.Location = new Point(464, 198);
             Divide.Name = "Divide";
+            Divide.Padding = new Padding(1);
             Divide.Size = new Size(50, 30);
             Divide.TabIndex = 17;
             Divide.Text = "/";
-            Divide.UseVisualStyleBackColor = true;
+            Divide.UseVisualStyleBackColor = false;
             Divide.Click += Divide_Click;
             // 
             // Multiply
             // 
+            Multiply.BackColor = SystemColors.GradientInactiveCaption;
+            Multiply.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Multiply.Location = new Point(464, 162);
             Multiply.Name = "Multiply";
+            Multiply.Padding = new Padding(1);
             Multiply.Size = new Size(50, 30);
             Multiply.TabIndex = 16;
             Multiply.Text = "*";
-            Multiply.UseVisualStyleBackColor = true;
+            Multiply.UseVisualStyleBackColor = false;
             Multiply.Click += Multiply_Click;
             // 
             // Subtract
             // 
+            Subtract.BackColor = SystemColors.GradientInactiveCaption;
+            Subtract.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Subtract.Location = new Point(464, 126);
             Subtract.Name = "Subtract";
+            Subtract.Padding = new Padding(1);
             Subtract.Size = new Size(50, 30);
             Subtract.TabIndex = 15;
             Subtract.Text = "-";
-            Subtract.UseVisualStyleBackColor = true;
+            Subtract.UseVisualStyleBackColor = false;
             Subtract.Click += Subtract_Click;
             // 
             // Add
             // 
+            Add.BackColor = SystemColors.GradientInactiveCaption;
+            Add.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Add.Location = new Point(464, 90);
             Add.Name = "Add";
+            Add.Padding = new Padding(1);
             Add.Size = new Size(50, 30);
             Add.TabIndex = 14;
             Add.Text = "+";
-            Add.UseVisualStyleBackColor = true;
+            Add.UseVisualStyleBackColor = false;
             Add.Click += Add_Click;
             // 
             // Equal
             // 
+            Equal.BackColor = Color.LightSteelBlue;
+            Equal.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Equal.Location = new Point(464, 234);
             Equal.Name = "Equal";
+            Equal.Padding = new Padding(1);
             Equal.Size = new Size(50, 30);
             Equal.TabIndex = 21;
             Equal.Text = "=";
-            Equal.UseVisualStyleBackColor = true;
+            Equal.UseVisualStyleBackColor = false;
             // 
             // Clear
             // 
+            Clear.BackColor = Color.LightSteelBlue;
+            Clear.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Clear.Location = new Point(408, 234);
             Clear.Name = "Clear";
+            Clear.Padding = new Padding(1);
             Clear.Size = new Size(50, 30);
             Clear.TabIndex = 20;
             Clear.Text = "AC";
-            Clear.UseVisualStyleBackColor = true;
+            Clear.UseVisualStyleBackColor = false;
             // 
             // Round
             // 
+            Round.BackColor = SystemColors.GradientInactiveCaption;
+            Round.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Round.Location = new Point(352, 234);
             Round.Name = "Round";
+            Round.Padding = new Padding(1);
             Round.Size = new Size(50, 30);
             Round.TabIndex = 19;
             Round.Text = "RND";
-            Round.UseVisualStyleBackColor = true;
+            Round.UseVisualStyleBackColor = false;
             // 
             // Negation
             // 
+            Negation.BackColor = SystemColors.GradientInactiveCaption;
+            Negation.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Negation.Location = new Point(296, 234);
             Negation.Name = "Negation";
+            Negation.Padding = new Padding(1);
             Negation.Size = new Size(50, 30);
             Negation.TabIndex = 18;
             Negation.Text = "+/-";
-            Negation.UseVisualStyleBackColor = true;
+            Negation.UseVisualStyleBackColor = false;
             // 
             // MemoryRecall
             // 
+            MemoryRecall.BackColor = SystemColors.GradientInactiveCaption;
+            MemoryRecall.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
+            MemoryRecall.ForeColor = SystemColors.ControlText;
             MemoryRecall.Location = new Point(520, 234);
             MemoryRecall.Name = "MemoryRecall";
+            MemoryRecall.Padding = new Padding(1);
             MemoryRecall.Size = new Size(50, 30);
             MemoryRecall.TabIndex = 26;
             MemoryRecall.Text = "MR";
-            MemoryRecall.UseVisualStyleBackColor = true;
+            MemoryRecall.UseVisualStyleBackColor = false;
             // 
             // MemoryRemove
             // 
+            MemoryRemove.BackColor = SystemColors.GradientInactiveCaption;
+            MemoryRemove.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             MemoryRemove.Location = new Point(520, 198);
             MemoryRemove.Name = "MemoryRemove";
+            MemoryRemove.Padding = new Padding(1);
             MemoryRemove.Size = new Size(50, 30);
             MemoryRemove.TabIndex = 25;
             MemoryRemove.Text = "M-";
-            MemoryRemove.UseVisualStyleBackColor = true;
+            MemoryRemove.UseVisualStyleBackColor = false;
             // 
             // MemoryAdd
             // 
+            MemoryAdd.BackColor = SystemColors.GradientInactiveCaption;
+            MemoryAdd.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             MemoryAdd.Location = new Point(520, 162);
             MemoryAdd.Name = "MemoryAdd";
+            MemoryAdd.Padding = new Padding(1);
             MemoryAdd.Size = new Size(50, 30);
             MemoryAdd.TabIndex = 24;
             MemoryAdd.Text = "M+";
-            MemoryAdd.UseVisualStyleBackColor = true;
+            MemoryAdd.UseVisualStyleBackColor = false;
             // 
             // Answer
             // 
+            Answer.BackColor = SystemColors.GradientInactiveCaption;
+            Answer.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Answer.Location = new Point(520, 126);
             Answer.Name = "Answer";
+            Answer.Padding = new Padding(1);
             Answer.Size = new Size(50, 30);
             Answer.TabIndex = 23;
             Answer.Text = "Ans";
-            Answer.UseVisualStyleBackColor = true;
+            Answer.UseVisualStyleBackColor = false;
             // 
             // Back
             // 
+            Back.BackColor = SystemColors.GradientInactiveCaption;
+            Back.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold);
             Back.Location = new Point(520, 90);
             Back.Name = "Back";
+            Back.Padding = new Padding(1);
             Back.Size = new Size(50, 30);
             Back.TabIndex = 22;
             Back.Text = "Back";
-            Back.UseVisualStyleBackColor = true;
+            Back.UseVisualStyleBackColor = false;
             // 
             // result
             // 
-            result.AutoSize = true;
+            result.BackColor = Color.SteelBlue;
             result.Location = new Point(296, 56);
             result.Name = "result";
-            result.Size = new Size(0, 15);
-            result.TabIndex = 27;
+            result.Size = new Size(274, 31);
+            result.TabIndex = 28;
             // 
             // Calculator
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(789, 477);
             Controls.Add(result);
             Controls.Add(MemoryRecall);
@@ -338,7 +415,6 @@
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -369,5 +445,6 @@
         private Button Answer;
         private Button Back;
         private Label result;
+        public Label result;
     }
 }
